1fd5c1d899b16a3ded522612a0e5bd70cd5cfc79
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-04-14 22:08:52 +0100

    v2 commit

--- app.js
+++ app.js
@@ -1,39 +1,127 @@
+// Requiring dependencies and Creating Variables
 const express = require("express");
 const bodyParser = require("body-parser");
-const day = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
-let newTodos = [];
-let workTodo = [];
-app.get("/", (req, res) => {
-  let currentDay = day.getDate();
-  res.render("index", { listTitle: currentDay, newItem: newTodos });
+// MongoDB
+// Start of mongoDB
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+// Schema and models
+const ItemsSchema = new mongoose.Schema({
+  name: String,
 });
+const Item = mongoose.model("Item", ItemsSchema);
+const code = new Item({
+  name: "Code something new.",
+});
+const eat = new Item({
+  name: "Eat",
+});
+const brush = new Item({
+  name: "Brush my teeth",
+});
+const defaultTodo = [brush, eat, code];
+// Work Items Schema
+const WorkSchema = new mongoose.Schema({
+  name: String,
+});
+const Work = mongoose.model("Work", WorkSchema);
 
-app.post("/", (req, res) => {
-  let newTodo = req.body.newItem;
-  if (req.body.listType === "Work List") {
-    workTodo.push(newTodo);
-    res.redirect("/work");
-  } else {
-    newTodos.push(newTodo);
-    res.redirect("/");
-  }
+// Item.insertMany(defaultTodo, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("Successfully Saved to DB.");
+//   }
+// });
+
+// end of MongoDB
+// Requests
+// Get requests
+app.get("/", (req, res) => {
+  Item.find({}, (err, items) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (items.length === 0) {
+        Item.insertMany(defaultTodo, (err) => {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Successfully Saved to DB.");
+          }
+          res.redirect("/");
+        });
+      } else {
+        res.render("index", {
+          listTitle: " Today ",
+          newItem: items,
+        });
+      }
+    }
+  });
 });
 app.get("/work", (req, res) => {
-  res.render("index", { listTitle: "Work List", newItem: workTodo });
-});
-app.post("/work", (req, res) => {
-  let newTodo = req.body.newItem;
-  workTodo.push(newTodo);
-  res.redirect("/work");
+  Work.find({}, (err, works) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("index", { listTitle: "Work List", newItem: works });
+    }
+  });
 });
 app.get("/about", (req, res) => {
   res.render("about");
 });
+// Post requests
+app.post("/", (req, res) => {
+  let newTodo = req.body.newItem;
+  if (req.body.listType === "Work List") {
+    const newItem = new Work({
+      name: newTodo,
+    });
+    Work.insertMany([newItem], (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully added new item to Work DB");
+      }
+    });
+    res.redirect("/work");
+  } else {
+    const newItem = new Item({
+      name: newTodo,
+    });
+    Item.insertMany([newItem], (err) => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log("Successfully added new item");
+      }
+    });
+    res.redirect("/");
+  }
+});
 
+app.post("/work", (req, res) => {
+  let newTodo = req.body.newItem;
+  const newItem = new Work({
+    name: newTodo,
+  });
+  Work.insertMany([newItem], (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Successfully added new item to Work DB");
+    }
+  });
+  res.redirect("/work");
+});
+
+// Listener
 app.listen(process.env.PORT || 8080, () => {
   console.log("Server started on port 8080");
 });
